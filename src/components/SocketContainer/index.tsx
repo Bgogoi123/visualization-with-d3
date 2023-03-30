4e769a056769b30211f3a852cd30e56677e8a7ec
Bgogoi123 <bgogoi055@gmail.com>
2023-03-30 12:37:45 +0530

    [feat][Connecting to Backend] - Changed backend URL

--- src/components/SocketContainer/index.tsx
+++ src/components/SocketContainer/index.tsx
@@ -9,14 +9,17 @@ const SocketContainer = ({ children }: { children: JSX.Element }) => {
   const [socket, setSocket] = useState<any>();
   // const ngrokURL =
   //   "https://8501-2405-201-a804-7cac-ad21-eb08-d984-4364.in.ngrok.io";
+  const renderURL = "https://sketchbook-backend.onrender.com";
 
   useEffect(() => {
-    setSocket(io("http://127.0.0.1:3000"));
-    // setSocket(
-    //   io(ngrokURL, {
-    //     transports: ["websocket"],
-    //   })
-    // );
+    // setSocket(io("http://127.0.0.1:3000"));
+
+    // Backend URL running at render.com
+    setSocket(
+      io(renderURL, {
+        transports: ["websocket"],
+      })
+    );
 
     return () => socket?.disconnect;
   }, [socket?.disconnect]);
